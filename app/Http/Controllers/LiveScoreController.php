5efe0098b853da62e2d8a5e61bf21bf81ec8497e
huy le <huyanhit@gmail.com>
2024-08-04 22:46:48 +0700

    update

--- app/Http/Controllers/LiveScoreController.php
+++ app/Http/Controllers/LiveScoreController.php
@@ -37,17 +37,13 @@ class LiveScoreController extends Controller
         if($request['matchId']){
             return response(['code'=> 0,
                 'system' => $this->getServerInfo(),
-                'data' => !empty(Cache::get('live-score-'.$request['matchId']) ?
-                    Cache::get('live-score-'.$request['matchId']):
-                    Livescore::where('matchId', $request['matchId'])->get())
+                'data' => Cache::get('live-score-'.$request['matchId'] ?? [])
             ]);
         }
 
         return response(['code'=> 0,
             'system' => $this->getServerInfo(),
-            'data'=> !empty(Cache::get('live-score'))?
-                Cache::get('live-score'):
-                Livescore::whereBetween('matchTime', [$from, $to])->get()
+            'data'=> Cache::get('live-score') ?? []
         ]);
     }
 
